82eb8201bf251be99f33736328bfa2268c8ff395
elsankary-02 <githup800900@gmail.com>
2025-02-11 22:25:37 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+     
     );
   }
 }
