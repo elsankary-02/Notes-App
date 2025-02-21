c1a47fdb36702b1706e4e1090cb05ab0aacb6cf1
elsankry02 <githup800900@gmail.com>
2025-02-21 11:38:36 +0200

    create FloatingActionWidget

--- lib/features/pages/home/widget/floating_action_widget.dart
+++ lib/features/pages/home/widget/floating_action_widget.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/core/constant/color_manger.dart';
+import 'package:notes_app/features/pages/home/widget/bottom_sheet_widget.dart';
 
-class FloatingActionButtonWidget extends StatelessWidget {
-  const FloatingActionButtonWidget({
+class FloatingActionWidget extends StatelessWidget {
+  const FloatingActionWidget({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return BottomSheetWidget();
+          },
+        );
+      },
       backgroundColor: ColorManger.kBlue,
       child: Icon(
         Icons.add,
