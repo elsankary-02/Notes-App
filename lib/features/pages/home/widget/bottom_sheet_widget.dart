f9cd2968ba0bdaa0e4b90fce7d78abeb91a30882
elsankry02 <githup800900@gmail.com>
2025-02-21 11:39:39 +0200

    create BottomSheetWidget

---
+++ lib/features/pages/home/widget/bottom_sheet_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/core/components/custom_botton.dart';
+import 'package:notes_app/core/components/custom_text_form_field.dart';
+
+class BottomSheetWidget extends StatefulWidget {
+  const BottomSheetWidget({
+    super.key,
+  });
+
+  @override
+  State<BottomSheetWidget> createState() => _BottomSheetWidgetState();
+}
+
+class _BottomSheetWidgetState extends State<BottomSheetWidget> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(),
+      child: Form(
+        key: formKey,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          children: [
+            // Titel
+            Padding(
+              padding: const EdgeInsets.only(top: 30, bottom: 20),
+              child: CustomTextFormField(
+                hintText: 'Titel',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'empty';
+                  } else {
+                    return 'valid';
+                  }
+                },
+              ),
+            ),
+            // Content
+            CustomTextFormField(
+              contentPadding: EdgeInsets.only(top: 40, bottom: 40, left: 10),
+              hintText: 'Content',
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'empty';
+                } else {
+                  return 'valid';
+                }
+              },
+            ),
+            CustomButton(
+              onTap: () {
+                formKey.currentState!.validate();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
