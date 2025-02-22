5c6d5e15f05de471e894fc4bb9165ff36a8a4b3d
elsankry02 <githup800900@gmail.com>
2025-02-22 10:59:34 +0200

    create Edit Note Page

---
+++ lib/features/pages/edit_notes/edit_note_page.dart
@@ -0,0 +1,49 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_app/core/components/custom_text_form_field.dart';
+import 'package:notes_app/features/pages/home/widget/search_widget.dart';
+import 'package:notes_app/features/providers/note_provider.dart';
+
+@RoutePage()
+class EditNotePage extends ConsumerWidget {
+  const EditNotePage({super.key, required this.index});
+  final int index;
+  @override
+  Widget build(BuildContext context, ref) {
+    final textTheme = Theme.of(context).textTheme;
+    final itemProvider = ref.watch(noteItemsProvider);
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        forceMaterialTransparency: true,
+        title: Text(
+          'Edit Note',
+          style: textTheme.headlineMedium,
+        ),
+        actions: [
+          // SearchIconWidget
+          SearchWidget(
+            icon: Icons.check,
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        children: [
+          SizedBox(height: 30),
+          // titel
+          CustomTextFormField(hintText: 'titel'),
+          SizedBox(height: 20),
+          // content
+          CustomTextFormField(
+            hintText: 'content',
+            maxLines: 4,
+          ),
+          SizedBox(height: 30),
+          Text(itemProvider[index].number.toString())
+        ],
+      ),
+    );
+  }
+}
